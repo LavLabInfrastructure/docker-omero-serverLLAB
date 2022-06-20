378301ddcff5f394d0b13bb6046d92779ffee024
barrettMCW <mjbarrett@mcw.edu>
2022-06-20 06:34:38 -0500

    found --retry flag

--- 89-initImporter.sh
+++ 89-initImporter.sh
@@ -2,12 +2,10 @@
 #80 LEVEL SCRIPTS ONLY RAN ONCE!
 #Creates importer with specified details
 initImporter() {
-    sleep 30s
     echo "CREATING IMPORTER USER"
-    omero login --sudo root -u root -w "${ROOTPASS:? "define ROOTPASS"}" -s localhost -p 4064
     omero group add ${IMPORTGROUP:? "define IMPORTGROUP"}
     omero user add importer OMERO IMPORTER --group-name ${IMPORTGROUP:?} -P "${IMPORTPASS:? "define IMPORTPASS"}"
     touch /OMERO/init.byte
 }
-#put in background til server starts
-initImporter &
+omero login --sudo root -u root -w "${ROOTPASS:? "define ROOTPASS"}" -s localhost -p 4064 --retry 120 && \
+initImporter 
